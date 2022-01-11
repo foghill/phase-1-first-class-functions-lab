11e22ed36757ac0768f3211bc4eadd86a732e6d6
benjamindean <ben@thefoghill.com>
2022-01-11 13:26:23 -0500

    commit tree e139445b1a37262a93c71aaf9fdc8490cac4d4fb

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const aryDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = (aryDrivers) => { 
+const aryd = aryDrivers.slice(0,2)
+return aryd
+}
+
+const returnLastTwoDrivers = (aryDrivers) => {
+    if (aryDrivers.length >= 2) {
+        return aryDrivers.slice(-2)
+    }
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (num) => {
+    return (fare) => num * fare;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+function selectDifferentDrivers(drivers, driversToReturn) {
+    return driversToReturn(drivers)
+}
